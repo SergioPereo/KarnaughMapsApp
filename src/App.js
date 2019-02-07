80a225272b219cee6d39b964aaa7584bc2f28825
SergioPereo <35266985+SergioPereo@users.noreply.github.com>
2019-02-07 08:24:13 -0600

    version 0.0.1

--- src/App.js
+++ src/App.js
@@ -6,20 +6,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="primary-component">
+          <div className="container">
+            <div className="component">
+              <h1 className="title is-1">
+                Input how many variables your truth table going to have
+              </h1>
+            </div>
+            <div className="component">
+              <input className="input is-primary" placeholder="1, 2, 3..." type="text"/>
+            </div>
+            <div className="component">
+              <div className="field is-grouped">
+                <div className="control">
+                  <button className="button is-primary">Create</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
